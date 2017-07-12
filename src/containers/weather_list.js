de6df79eb4ecf574d1ede1988942894f62b8deb9
rizasatyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-07-12 23:05:16 +0700

    Add files via upload

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,11 +6,12 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     return (
-      <tr key={name}>
-        <td> {name}</td>
+      <tr>
+        <td> {name} </td>
       </tr>
     );
   }
+
   render() {
     return (
       <table className="table table-hover">
