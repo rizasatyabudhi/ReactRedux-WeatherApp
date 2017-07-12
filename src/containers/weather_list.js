10afe986526a6efdb5e2f5f02d3adf21d25e0278
Unknown <riza.ega.s@mail.ugm.ac.id>
2017-07-12 18:12:49 +0700

    create WeatherList container

---
+++ src/containers/weather_list.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class WeatherList extends Component {
+  render() {
+    return (
+      <table className="table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+          </tr>
+        </thead>
+
+        <tbody />
+      </table>
+    );
+  }
+}
+
