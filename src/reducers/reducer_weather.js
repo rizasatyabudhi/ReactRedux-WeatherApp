5d162cf14cc888916e2265e3f2263396f973d57d
Unknown <riza.ega.s@mail.ugm.ac.id>
2017-07-12 18:04:23 +0700

    update reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,11 @@
-export default function (state = null, action) {
-  console.log('action received', action);
+import FETCH_WEATHER from '../actions/index';
+
+export default function (state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // Concat our old array of city with new cities
+      // [city1, city2, city3] , NOT [city1,[city2,[city3]]]
+      return [action.payload.data, ...state];
+  }
   return state;
 }
