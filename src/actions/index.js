a03646e7992c865f5aa95d899d4c166d0cb2fd2c
rizasatyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-07-12 18:04:32 +0700

    Add files via upload
    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+// We use axios for the AJAX request
+import axios from 'axios';
+
+const API_KEY = 'a66437ed6ae996440e8cddee3fdc6749';
+const URL_ROOT = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${URL_ROOT}&q=${city},Indonesia`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
---
+++ src/containers/search_bar.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+// These imports are used to connecct SearchBar container to Redux
+// in order to use the ActionCreaator ( fetchWeather)
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
+class SearchBar extends Component {
+    // Initial state
+  constructor(props) {
+    super(props);
+
+    // Make our default state as empty string
+    this.state = { term: '' };
+
+    // We need to bind "this" to fix the (cannot find setState of undefined)
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(event) {
+    // Vanilla JS thing, not react
+    this.setState({ term: event.target.value });
+  }
+
+
+  onFormSubmit(event) {
+    // prevents the <form> from re-submitting
+    event.preventDefault();
+    // When the user submit the form, we want to fire up the fetchWeather ActionCreator
+    this.props.fetchWeather(this.state.term);
+    // After we kickoff the search, we want to clear up that search input
+    this.setState({ term: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a five day forecast in your favorite cities"
+          className="form-control"
+
+          // turn this into controlled component
+          // the input value come from this.state.term
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary"> Submit </button>
+        </span>
+
+      </form>
+    );
+  }
+}
+
+// These code below is used to "glue" our fetchWeather ActionCreator to SearchBar component
+// so the fetchWeather will be accessible through this.props.fetchWeather
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// We want our mapDispatchToProps as our second argument, so we use "null" as our first argument
+// Since we don't need mapStateToProps (first argument)
+export default connect(null, mapDispatchToProps)(SearchBar);
